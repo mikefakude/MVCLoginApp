47cc9cdd765692cfcac17cf257b91e94d8df6637
Xolani <xolanif@bumblebee.co.za>
2018-11-30 16:09:14 +0200

    new changes

--- MVCLogin/Models/User.cs
+++ MVCLogin/Models/User.cs
@@ -9,13 +9,15 @@
 
 namespace MVCLogin.Models
 {
-    using System;
-    using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class User
     {
         public int UserID { get; set; }
+        [DisplayName("User Name")]
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string PassWord { get; set; }
     }
 }
